e5d99fb3989b3f7661b87af7094dd6e43ced0051
Andrew Pashinnik <tobirawork@gmail.com>
2019-03-05 08:53:50 +0200

    chore(eslint): update vue rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,23 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 2,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint',
