05dc090981c3fc8a62eaa3da16f24294cc13871e
Andrew Pashinnik <tobirawork@gmail.com>
2019-03-07 13:12:33 +0200

    feat(components): add FreeDrawingCanvas

---
+++ src/components/FreeDrawingCanvas.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <canvas
+      id="canva"
+      :width="size"
+      :height="size"
+      class="canva" />
+  </div>
+</template>
+
+<script>
+import { fabric } from 'fabric';
+
+export default {
+  name: 'FreeDrawingCanvas',
+
+  props: {
+    size: {
+      type: Number,
+      default: 400,
+    },
+  },
+
+  data: () => ({
+    canvas: null,
+    isDrawing: false,
+    path: [],
+    lastPoint: { x: -1, y: -1 },
+  }),
+
+  watch: {
+    lastPoint(newPoint) {
+      if (this.isDrawing) this.path.push(newPoint);
+      this.$emit('change-point', newPoint);
+    },
+  },
+
+  mounted() {
+    this.canvas = new fabric.Canvas('canva', {
+      isDrawingMode: true,
+      backgroundColor: '#efefef',
+    });
+    this.canvas.freeDrawingBrush.color = 'black';
+    this.canvas.freeDrawingBrush.width = 4;
+    this.canvas.renderAll();
+
+    this.canvas.on('mouse:move', this.onMouseMove);
+    this.canvas.on('mouse:down', this.onMouseDown);
+    this.canvas.on('mouse:up', this.onMouseUp);
+
+    this.$emit('change-point', this.lastPoint);
+  },
+
+  beforeDestroy() {
+    this.canvas.dispose();
+  },
+
+  methods: {
+    onMouseDown() {
+      this.isDrawing = true;
+    },
+
+    onMouseMove({ absolutePointer }) {
+      if (this.isDrawing) {
+        this.lastPoint.x = Math.round(absolutePointer.x);
+        this.lastPoint.y = Math.round(absolutePointer.y);
+      }
+    },
+
+    onMouseUp() {
+      this.$store.dispatch('pushPath', this.path);
+
+      this.isDrawing = false;
+      this.path = [];
+    },
+  },
+};
+</script>
+
+<style>
+#canva {
+  margin: auto;
+}
+</style>
--- src/views/Drawing.vue
+++ src/views/Drawing.vue
@@ -2,68 +2,29 @@
   <div class="drawing">
     <div class="columns">
       <div class="column">
-        <canvas
-          id="canvas"
-          width="400"
-          height="400"
-          class="canvas" />
+        <free-drawing-canvas :size="500" @change-point="onChangeBrushPoint" />
+        <span>
+          Current Brush Point (x: {{ point.x }}; y: {{ point.y }})
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { fabric } from 'fabric';
+import FreeDrawingCanvas from '../components/FreeDrawingCanvas.vue';
 
 export default {
   name: 'Drawing',
-  components: {},
+  components: { FreeDrawingCanvas },
 
   data: () => ({
-    canvas: null,
-    isDrawing: false,
-    path: [],
+    point: { x: NaN, y: NaN },
   }),
 
-  mounted() {
-    this.canvas = new fabric.Canvas('canvas', {
-      isDrawingMode: true,
-      backgroundColor: '#efefef',
-    });
-    this.canvas.freeDrawingBrush.color = 'black';
-    this.canvas.freeDrawingBrush.width = 4;
-    this.canvas.renderAll();
-
-    this.canvas.on('mouse:move', this.onMouseMove);
-    this.canvas.on('mouse:down', this.onMouseDown);
-    this.canvas.on('mouse:up', this.onMouseUp);
-  },
-
   methods: {
-    onMouseDown() {
-      this.isDrawing = true;
-    },
-
-    onMouseMove(e) {
-      if (this.isDrawing) {
-        const coords = e.absolutePointer;
-        const lastCoord = this.path[this.path.length - 1];
-
-        if (this.path.length === 0) {
-          this.path.push(coords);
-          return;
-        }
-
-        if (lastCoord.x !== coords.x || lastCoord.y !== coords.y) {
-          this.path.push(coords);
-        }
-      }
-    },
-
-    onMouseUp() {
-      this.isDrawing = false;
-      this.$store.dispatch('pushPath', this.path);
-      this.path = [];
+    onChangeBrushPoint(p) {
+      this.point = p;
     },
   },
 };
