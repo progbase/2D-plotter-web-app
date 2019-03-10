aca55f5d9bc6e93d13fde3fa9bf4d34d783ae024
Andrew Pashinnik <tobirawork@gmail.com>
2019-03-10 09:28:39 +0200

    feat(components): add ImageCard

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="image-card">
+    <img
+      class="image"
+      :src="src"
+      @click="$emit('click')">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageCard',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.image-card {
+  background-color: #fff;
+  padding: 2px;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+}
+
+.image {
+  width: 100%;
+}
+</style>
