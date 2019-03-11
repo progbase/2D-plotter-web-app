53f8b7034a23e88d0c0f3a558e318352a9c017d0
Andrew Pashinnik <tobirawork@gmail.com>
2019-03-11 18:33:38 +0200

    refactor(components/Navbar)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,51 +1,39 @@
 <template>
-  <div id="nav">
-    <nav class="navbar is-warning">
+  <nav
+    class="navbar has-shadow is-spaced is-warning"
+    role="navigation"
+    aria-label="main navigation">
+    <div class="container">
       <div class="navbar-brand">
+        <a class="navbar-item" href="https://github.com/orgs/progbase/teams/baboo-industry">
+          <p class="title">Baboo</p>
+        </a>
+
         <a
-          class="navbar-item"
-          href="https://github.com/orgs/progbase/teams/baboo-industry">
-          <h1>
-            Baboo Industry team
-          </h1>
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample">
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
         </a>
       </div>
 
       <div class="navbar-menu">
-        <div class="navbar-start">
-          <router-link
-            class="navbar-item"
-            to="/">
+        <div class="navbar-end">
+          <router-link to="/" class="navbar-item">
             Home
           </router-link>
-        </div>
 
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="field is-grouped">
-              <p class="control">
-                <a class="button is-success" @click="connect">
-                  <span class="icon">
-                    <i class="fas fa-ok" />
-                  </span>
-                  <span>Connect</span>
-                </a>
-              </p>
-
-              <p class="control">
-                <a class="button is-danger" @click="disconnect">
-                  <span class="icon">
-                    <i class="fas fa-close" />
-                  </span>
-                  <span>Disconnect</span>
-                </a>
-              </p>
-            </div>
-          </div>
+          <router-link to="/real-time-drawing" class="navbar-item">
+            Drawing
+          </router-link>
         </div>
       </div>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -65,6 +53,9 @@ export default {
 </script>
 
 
-<style lang="postcss">
-
+<style lang="stylus">
+.navbar {
+  /*height: 60px;*/
+  /*background-color: #ac58cc !important;*/
+}
 </style>
